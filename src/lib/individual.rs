2654a0b6c58ac423252ecbe7531b1fdd75e4fa85
Vasco Ferreira <vasco_mmf@hotmail.com>
2022-09-05 22:47:01 +0000

    Removed normalized fitness and refactored code

--- src/lib/individual.rs
+++ src/lib/individual.rs
@@ -10,7 +10,6 @@ pub struct Individual {
     route: Route,
     total_distance: f64,
     fitness: f64,
-    normalized_fitness: f64,
 }
 
 impl Individual {
@@ -20,7 +19,6 @@ impl Individual {
             route,
             total_distance: 0.0,
             fitness: 0.0,
-            normalized_fitness: 0.0,
         }
     }
 
@@ -28,16 +26,8 @@ impl Individual {
         &self.route
     }
 
-    pub fn set_route(&mut self, new_route: Route) {
-        self.route = new_route
-    }
-
     pub fn get_fitness(&self) -> f64 {
         self.fitness
-    }
-
-    pub fn get_normalized_fitness(&self) -> f64 {
-        self.normalized_fitness
     }
 
     /// Shuffle the route
@@ -46,10 +36,9 @@ impl Individual {
     }
 
     /// Update the total distance and fitness values
-    pub fn update(&mut self, fitness_sum: f64) {
+    pub fn update(&mut self) {
         self.total_distance = self.route.total_distance();
         self.fitness = 1.0 / (1.0 + self.total_distance.powf(8.0));
-        self.normalized_fitness = self.fitness / fitness_sum;
     }
 
     /// Mutate the individual
@@ -77,14 +66,13 @@ impl Individual {
         }
         println!("- Total distance: {}", self.route.total_distance());
         println!("- Fitness: {}", self.fitness);
-        println!("- Normalized Fitness: {}", self.normalized_fitness);
         println!("----------------------------------------------");
     }
 
     /// combine a pair of individuals to generate a new route
     pub fn crossover(rng: &mut ChaCha20Rng,
                      parent_1: &Individual,
-                     parent_2: &Individual) -> Route {
+                     parent_2: &Individual) -> Individual {
 
         let p_1 = parent_1.get_route().get_cities();
         let p_2 = parent_2.get_route().get_cities();
@@ -92,19 +80,17 @@ impl Individual {
         let split_index: usize = rng.gen_range(0, p_1.len());
 
         let mut new_route: Vec<City> = Vec::new();
-        let mut cur_index = 0;
-        while cur_index < split_index {
-            new_route.push(p_1[cur_index].clone());
-            cur_index += 1;
+        for i in 0..split_index {
+            new_route.push(p_1[i].clone());
         }
 
         for i in 0..p_2.len() {
             let cur_city = &p_2[i];
-            if !new_route.iter().any(|city| &city == &cur_city) {
+            if !new_route.contains(&cur_city) {
                 new_route.push(cur_city.clone());
             }
         }
 
-        Route::new(new_route)
+        Individual::new(Route::new(new_route))
     }
 }
